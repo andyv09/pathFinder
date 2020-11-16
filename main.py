8bc8690dcb92064ea59d353989c46fd81010e6f5
andrazv09 <andraz.vrecko@gmail.com>
2020-11-16 18:50:31 +0100

    -Added Dijsktra and A*

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import pygame, sys
-from collections import deque
+from queue import PriorityQueue
+import pygame, time
 
-size = (width, height) = 1280, 720
-GRID_SIZE = 32
+size = (width, height) = 960, 960
+GRID_SIZE = 64
 w = width//GRID_SIZE
 h = height//GRID_SIZE
 grid = []
@@ -14,6 +14,10 @@ class Node:
         self.prev = None
         self.wall = False
         self.visited = False
+        self.f = float('inf')
+        self.g = float('inf')
+        self.h = float('inf')
+
     def draw(self, win, val=0):
         if val == 1:
             pygame.draw.rect(win, (0, 255, 0), (self.x * w, self.y * h, w - 1, h - 1))
@@ -30,7 +34,8 @@ class Node:
             pygame.draw.rect(win, (46, 47, 48), (self.x * w, self.y * h, w - 1, h - 1))
         else:
             pygame.draw.rect(win, (255, 200, 200), (self.x * w, self.y * h, w - 1, h - 1))
-
+    def get_pos(self):
+        return (self.x, self.y)
     def add_neighbors(self, G):
         if self.x < GRID_SIZE - 1:
             self.neighbors.append(G[self.x + 1][self.y])
@@ -60,6 +65,10 @@ queue = []
 visited = []
 stack = []
 path = []
+#ASTAR
+count = 0
+open_set = PriorityQueue()
+open_set_hash = []
 
 win = pygame.display.set_mode(size)
 pygame.display.set_caption("Test")
@@ -72,23 +81,47 @@ def clickBox(pos):
 
 pathFound = False
 
+def clear():
+    global queue, visited, stack, path, count, open_set_hash, open_set, start, end, pathFound
+    queue = []
+    visited = []
+    stack = []
+    path = []
+    count = 0
+    open_set = PriorityQueue()
+    open_set_hash = []
+    pathFound = False
+    # Init GRID
+    grid.clear()
+    for i in range(GRID_SIZE):
+        arr = []
+        for j in range(GRID_SIZE):
+            arr.append(Node(i, j))
+        grid.append(arr)
+    # Create neighbors
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            grid[i][j].add_neighbors(grid)
+    start = None
+    end = None
+
 def DFS():
     global pathFound
     if len(stack) > 0:
         node = stack.pop(0)
-        if node == end:
-            temp = node
-            while temp.prev:
-                path.append(temp.prev)
-                temp = temp.prev
-            print('Found end')
-            pathFound = True
         #continue search
         if pathFound == False:
             for i in node.neighbors:
                 if i.visited == False and i.wall == False:
                     i.visited = True
                     i.prev = node
+                    if i == end:
+                        temp = i
+                        while temp.prev:
+                            path.append(temp.prev)
+                            temp = temp.prev
+                        print('Found end')
+                        pathFound = True
                     stack.insert(0, i)
 
 def BFS():
@@ -109,12 +142,62 @@ def BFS():
                     i.visited = True
                     i.prev = node
                     stack.append(i)
+def dist(p1, p2):
+    x1, y1 = p1
+    x2, y2 = p2
+    return abs(x1 - x2) + abs(y1 - y2)
 
 def ASTAR():
-    pass
+    global count, pathFound
+    if not open_set.empty():
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+
+        if current == end:
+            pathFound = True
+            temp = current
+            while temp.prev:
+                path.append(temp.prev)
+                temp = temp.prev
+
+        if not pathFound:
+            for i in current.neighbors:
+                if i.wall == False:
+                    temp_g = current.g + 1
+                    if temp_g < i.g:
+                        i.prev = current
+                        i.g = temp_g
+                        i.f = temp_g + dist(i.get_pos(), end.get_pos())
+                        if i not in open_set_hash:
+                            count += 1
+                            open_set.put((i.f, count, i))
+                            open_set_hash.append(i)
+                            i.visited = True
 
 def DIJKSTRA():
-    pass
+    global count, pathFound
+    if not open_set.empty():
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
+        if current == end:
+            pathFound = True
+            temp = current
+            while temp.prev:
+                path.append(temp.prev)
+                temp = temp.prev
+        if not pathFound:
+            for i in current.neighbors:
+                if i.wall == False:
+                    temp_g = current.g + 1
+                    if temp_g < i.g:
+                        i.prev = current
+                        i.g = temp_g
+                        i.f = temp_g
+                        if i not in open_set_hash:
+                            count += 1
+                            open_set.put((i.f, count, i))
+                            open_set_hash.append(i)
+                            i.visited = True
 
 def main():
     startAlgorithm = False
@@ -135,19 +218,25 @@ def main():
                         x = pos[0] // w
                         y = pos[1] // h
                         start = grid[x][y]
-                        #DFS
+                        start.g = 0
+                        start.h = 0
+                        #DFS / BFS
                         start.visited = True
                         stack.append(start)
-
+                        #ASTAR
+                        open_set.put((0, 0, start))
+                        open_set_hash.append(start)
                     elif end == None:
                         pos = pygame.mouse.get_pos()
                         x = pos[0] // w
                         y = pos[1] // h
                         end = grid[x][y]
+                        start.f = dist(start.get_pos(), end.get_pos())
+
             if event.type == pygame.MOUSEMOTION and not startAlgorithm:
                 if pygame.mouse.get_pressed()[0]:
                     clickBox(pygame.mouse.get_pos())
-            if event.type == pygame.KEYDOWN and not startAlgorithm:
+            if event.type == pygame.KEYDOWN:
                 #H DFS
                 if event.key == 104:
                     startAlgorithm = True
@@ -163,6 +252,8 @@ def main():
                 if event.key == 108:
                     startAlgorithm = True
                     algorithm = 3
+                if event.key == 99:
+                    clear()
 
         if startAlgorithm:
             if start == None or end == None:
@@ -188,5 +279,7 @@ def main():
                 if node in path and node != start:
                     node.draw(win, val=3)
         pygame.display.flip()
+        if algorithm != 1 or algorithm != 3:
+            time.sleep(.01)
 
 main()
