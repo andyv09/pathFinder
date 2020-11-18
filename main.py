7d089556a2b898f2d2f66c68e6c5a13d85c631ea
andrazv09 <andraz.vrecko@gmail.com>
2020-11-18 12:30:03 +0100

    -Added Prim Algorithm for Maze Generation

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 from queue import PriorityQueue
-import pygame, time
+import numpy as np
+import pygame, time, random
 
-size = (width, height) = 960, 960
-GRID_SIZE = 32
-w = width//GRID_SIZE
-h = height//GRID_SIZE
-grid = []
 class Node:
     def __init__(self, x, y):
         self.x = x
@@ -38,7 +34,12 @@ class Node:
             pygame.draw.rect(win, (255, 200, 200), (self.x * w, self.y * h, w - 1, h - 1))
             pygame.draw.circle(win, (0,255,255), (self.x * w + w // 2, self.y * h + h // 2), w // 3)
         elif self.wall == True:
-            pygame.draw.rect(win, (0, 55, 250), (self.x * w, self.y * h, w - 1, h - 1))
+            pygame.draw.rect(win, (255, 200, 200), (self.x * w, self.y * h, w - 1, h - 1))
+            pygame.draw.rect(win, (0, 55, 250), (self.x * w + (w - 1 - self.pathSizeW)/2, self.y * h + (h - 1 - self.pathSizeH)/2, self.pathSizeW, self.pathSizeH))
+            if self.pathSizeH < (h-1):
+                self.pathSizeH += (h-1)*(1/self.growthDuration)
+            if self.pathSizeW < (w-1):
+                self.pathSizeW += (w-1)*(1/self.growthDuration)
         elif self.visited == True:
             pygame.draw.rect(win, (0, 204, 204), (self.x * w, self.y * h, w - 1, h - 1))
         else:
@@ -55,6 +56,12 @@ class Node:
         if self.y > 0:
             self.neighbors.append(G[self.x][self.y - 1])
 
+size = (width, height) = 990, 990
+GRID_SIZE = 33
+GRAPH_SIZE = (GRID_SIZE - 1)/2
+w = width//GRID_SIZE
+h = height//GRID_SIZE
+grid = []
 start = None
 end = None
 
@@ -70,6 +77,7 @@ for i in range(GRID_SIZE):
     for j in range(GRID_SIZE):
         grid[i][j].add_neighbors(grid)
 
+pathFound = False
 queue = []
 visited = []
 stack = []
@@ -80,19 +88,26 @@ count = 0
 open_set = PriorityQueue()
 open_set_hash = []
 
+maze = np.zeros((GRID_SIZE, GRID_SIZE), dtype=np.uint8)
 win = pygame.display.set_mode(size)
 pygame.display.set_caption("Pathfinding Visualizer")
-
 
 def clickBox(pos):
     x = pos[0] // w
     y = pos[1] // h
     grid[x][y].wall = True
 
-pathFound = False
+def deleteWall(pos):
+    x = pos[0] // w
+    y = pos[1] // h
+    grid[x][y].wall = False
+    grid[x][y].pathSizeH = (h - 1) * (1 / grid[x][y].growthDuration)
+    grid[x][y].pathSizeW = (w - 1) * (1 / grid[x][y].growthDuration)
+
 
 def clear():
-    global queue, visited, stack, path, count, open_set_hash, open_set, start, end, pathFound, drawPath
+    global queue, visited, stack, path, count, open_set_hash, open_set, start, end, pathFound, drawPath, maze
+    maze = np.zeros((GRID_SIZE, GRID_SIZE), dtype=np.uint8)
     drawPath = []
     queue = []
     visited = []
@@ -134,8 +149,9 @@ def reset():
             grid[i][j].f = float('inf')
             grid[i][j].g = float('inf')
             grid[i][j].h = float('inf')
+            grid[i][j].pathSizeH = (h-1) * (1/grid[i][j].growthDuration)
+            grid[i][j].pathSizeW = (w-1) * (1/grid[i][j].growthDuration)
     start.visited = True
-
     end.prev = None
     start.prev = None
     start.visited = True
@@ -146,6 +162,55 @@ def reset():
     # ASTAR
     open_set.put((0, 0, start))
     open_set_hash.append(start)
+
+_dir_two = [
+            lambda x, y: (x + 2, y),
+            lambda x, y: (x - 2, y),
+            lambda x, y: (x, y - 2),
+            lambda x, y: (x, y + 2)
+        ]
+_dir_one = [
+            lambda x, y: (x + 1, y),
+            lambda x, y: (x - 1, y),
+            lambda x, y: (x, y - 1),
+            lambda x, y: (x, y + 1)
+        ]
+_range  = list(range(4))
+def shuffle():
+    random.shuffle(_range)
+    return _range
+
+def isInGrid(x, y):
+    ret = x < 0 or y < 0 or x >= GRID_SIZE or y >= GRID_SIZE
+    return ret
+
+def prim():
+    frontier = []
+    x = random.randint(0, GRID_SIZE - 2)+1
+    y = random.randint(0, GRID_SIZE - 2)+1
+    maze[x,y] = 1 #0 - WALL, 1 - EMPTY, 2 - FRONTIER
+
+    for direction in _dir_two:
+        tx,ty = direction(x, y)
+        if not isInGrid(tx, ty):
+            frontier.append((tx, ty))
+            maze[tx, ty] = 2
+
+    while frontier:
+        x, y = frontier.pop(random.randint(0, len(frontier) - 1))
+        for idx in shuffle():
+            tx, ty = _dir_two[idx](x, y)
+            if not isInGrid(tx, ty) and maze[tx, ty] == 1:
+                maze[x, y] = maze[_dir_one[idx](x, y)] = 1
+                break
+        for direction in _dir_two:
+            tx, ty = direction(x, y)
+            if not isInGrid(tx, ty) and maze[tx, ty] == 0:
+                frontier.append((tx, ty))
+                maze[tx, ty] = 2
+    return True
+
+
 
 def DFS():
     global pathFound
@@ -241,67 +306,12 @@ def DIJKSTRA():
                             open_set_hash.append(i)
                             i.visited = True
 
-def main():
-    startAlgorithm = False
-    algorithm = 0
+class Program:
+    def __init__(self):
+        self.algorithm = 0
+        self.startAlgorithm = False
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                return
-            if event.type == pygame.MOUSEBUTTONUP and not startAlgorithm:
-                if event.button == 1:
-                    clickBox(pygame.mouse.get_pos())
-                if event.button == 3:
-                    global start, end
-                    if start == None:
-                        pos = pygame.mouse.get_pos()
-                        x = pos[0] // w
-                        y = pos[1] // h
-                        start = grid[x][y]
-                        start.g = 0
-                        start.h = 0
-                        #DFS / BFS
-                        start.visited = True
-                        stack.append(start)
-                        #ASTAR
-                        open_set.put((0, 0, start))
-                        open_set_hash.append(start)
-                    elif end == None:
-                        pos = pygame.mouse.get_pos()
-                        x = pos[0] // w
-                        y = pos[1] // h
-                        end = grid[x][y]
-                        start.f = dist(start.get_pos(), end.get_pos())
-
-            if event.type == pygame.MOUSEMOTION and not startAlgorithm:
-                if pygame.mouse.get_pressed()[0]:
-                    clickBox(pygame.mouse.get_pos())
-            if event.type == pygame.KEYDOWN:
-                #H DFS
-                if event.key == 104:
-                    startAlgorithm = True
-                    algorithm = 0
-                #J BFS
-                if event.key == 106:
-                    startAlgorithm = True
-                    algorithm = 1
-                #K A*
-                if event.key == 107:
-                    startAlgorithm = True
-                    algorithm = 2
-                #L DIJKSTRAj
-                if event.key == 108:
-                    startAlgorithm = True
-                    algorithm = 3
-                if event.key == 99:
-                    clear()
-                    startAlgorithm = False
-                if event.key == 118:
-                    reset()
-                    startAlgorithm = False
-
+    def runAlgorithm(self, startAlgorithm, algorithm):
         if startAlgorithm:
             if start == None or end == None:
                 startAlgorithm = False
@@ -313,9 +323,16 @@ def main():
                 ASTAR()
             elif algorithm == 3:
                 DIJKSTRA()
-        if len(path) > 0 and (len(stack) == 0 or len(open_set_hash) == 0):
-            drawPath.append(path.pop(len(path)-1))
 
+    def sleep(self, algorithm):
+        if algorithm != 1 or algorithm != 3:
+            time.sleep(.01)
+        if len(drawPath) > 0:
+            time.sleep(.01)
+
+    def draw(self):
+        if len(path) > 0 and (len(stack) == 0 or len(open_set_hash) == 0):
+            drawPath.append(path.pop(len(path) - 1))
         win.fill((0, 20, 20))
         for i in range(GRID_SIZE):
             for j in range(GRID_SIZE):
@@ -331,8 +348,80 @@ def main():
                     if node in stack or node in open_set_hash:
                         node.draw(win, val=4)
         pygame.display.flip()
-        if algorithm != 1 or algorithm != 3:
-            time.sleep(.01)
-        if len(drawPath) > 0:
-            time.sleep(.01)
-main()
+
+    def mainLoop(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    return
+                if event.type == pygame.MOUSEBUTTONUP and not self.startAlgorithm:
+                    if event.button == 1:
+                        clickBox(pygame.mouse.get_pos())
+                    if event.button == 2:
+                        deleteWall(pygame.mouse.get_pos())
+                    if event.button == 3:
+                        global start, end
+                        if start == None:
+                            pos = pygame.mouse.get_pos()
+                            x = pos[0] // w
+                            y = pos[1] // h
+                            start = grid[x][y]
+                            start.g = 0
+                            start.h = 0
+                            #DFS / BFS
+                            start.visited = True
+                            stack.append(start)
+                            #ASTAR
+                            open_set.put((0, 0, start))
+                            open_set_hash.append(start)
+                        elif end == None:
+                            pos = pygame.mouse.get_pos()
+                            x = pos[0] // w
+                            y = pos[1] // h
+                            end = grid[x][y]
+                            start.f = dist(start.get_pos(), end.get_pos())
+
+                if event.type == pygame.MOUSEMOTION and not self.startAlgorithm:
+                    if pygame.mouse.get_pressed()[0]:
+                        clickBox(pygame.mouse.get_pos())
+                if event.type == pygame.KEYDOWN:
+                    #H DFS
+                    if event.key == 104:
+                        self.startAlgorithm = True
+                        self.algorithm = 0
+                    #J BFS
+                    if event.key == 106:
+                        self.startAlgorithm = True
+                        self.algorithm = 1
+                    #K A*
+                    if event.key == 107:
+                        self.startAlgorithm = True
+                        self.algorithm = 2
+                    #L DIJKSTRA
+                    if event.key == 108:
+                        self.startAlgorithm = True
+                        self.algorithm = 3
+                    if event.key == 99:
+                        clear()
+                        self.startAlgorithm = False
+                    if event.key == 118:
+                        reset()
+                        self.startAlgorithm = False
+                    if event.key == 97:
+
+                        clear()
+                        prim()
+                        for i in range(GRID_SIZE):
+                            for j in range(GRID_SIZE):
+                                if maze[i][j] == 1:
+                                    grid[i][j].wall = False
+                                else:
+                                    grid[i][j].wall = True
+
+            self.runAlgorithm(self.startAlgorithm, self.algorithm)
+            self.draw()
+            self.sleep(self.algorithm)
+
+program = Program()
+program.mainLoop()
